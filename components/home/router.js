c8c29abf9d78eeeeb176ef7f8d58b15ff1889de1
Glitch (hello-webpage) <none>
2020-03-21 12:59:01 +0000

    🚍🐈 Checkpoint ./components/home/router.js:5329262/1 ./views/view.pug:5329262/191 ./components/home/controller.js:5329262/596

--- components/home/router.js
+++ components/home/router.js
@@ -2,6 +2,6 @@ const express = require('express')
 const router = express.Router();
 const controller = require('./controller');
 
-router.get('/:theme', controller.GET);
+router.get('/:theme?', controller.GET);
 
 module.exports = router;
