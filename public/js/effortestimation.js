4558c4dd0805f0370857126b1cfdea4a4c04015c
Glitch (hello-webpage) <none>
2020-03-28 16:20:19 +0000

    📻🔨 Checkpoint ./public/js/effortestimation.js:5329262/747

--- public/js/effortestimation.js
+++ public/js/effortestimation.js
@@ -1,6 +1,22 @@
 let approach = document.getElementById('approach')
-let 
+let category = document.getElementById('category')
 
-approach.onchange = () => {
-  
+approach.onchange = (e) => {
+  switch (e.srcElement.value) {
+    case 'analogy':
+    case 'parametric':
+    case 'size':
+      category.value = 'formal';
+      break;
+    case 'mechanical':
+    case 'judgmental':
+      category.value = 'combination';
+      break;
+    case 'wbs':
+    case 'group':
+      category.value = 'expert';
+      break;
+    default:
+      break;
+  }
 }
